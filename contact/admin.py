d11aa1542115d2adf358cf47d534e53c8e591a8e
eduardoqmf <eduardo.qmf@gmail.com>
2024-05-03 21:39:57 -0300

    adding db contacts

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from contact import models
+
+
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'phone', 'email',)
+    ordering = ('-id',)
+    search_fields = ('id', 'first_name', 'last_name',)
+    list_filter = ('created_date',)
+    list_per_page = 10
+    list_max_show_all = 100
+    list_display_links = ('id', 'first_name', 'last_name',)
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+# first_name (string), last_name (string), phone (string)
+# email (email), created_date (date), description (text)
+# category (foreign key), show(boolean),
+# picture (image)
+# owner(foreign key)
+
+# class Category(models.Model):
+#     id = models.AutoField(primary_key=True)
+#     name = models.CharField(max_length=255)
+
+
+# class Owner(models.Model):
+#     id = models.AutoField(primary_key=True)
+#     name = models.CharField(max_length=255)
+
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50, blank=True)
+    created_date = models.DateField(default=timezone.now)
+    description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    # id_category = models.ForeignKey(Category[id], on_delete=models.CASCADE)
+    # id_owner = models.ForeignKey(Owner.id, on_delete=models.CASCADE)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
