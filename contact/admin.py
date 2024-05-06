ddc1f891c0b09e401ac3243c6f1a1944d5edb4ee
eduardoqmf <eduardo.qmf@gmail.com>
2024-05-05 21:32:47 -0300

    creating view for one contact

--- contact/admin.py
+++ contact/admin.py
@@ -5,12 +5,13 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('id', 'first_name', 'last_name', 'phone', 'email',)
+    list_display = ('id', 'first_name', 'last_name', 'phone', 'email', 'show',)
     ordering = ('-id',)
     search_fields = ('id', 'first_name', 'last_name',)
     list_filter = ('created_date',)
     list_per_page = 10
     list_max_show_all = 100
+    list_editable = ('show',)
     list_display_links = ('id', 'first_name', 'last_name',)
 
 
--- contact/urls.py
+++ contact/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from . import views
+from contact import views
 
 app_name = 'contact'
 
 urlpatterns = [
+    path('<int:contact_id>/', views.contact, name='contact'),
     path('', views.index, name='index'),
 ]
