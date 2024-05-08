e80ca4862ae994dc2995e8ee5adb0274767774ca
eduardoqmf <eduardo.qmf@gmail.com>
2024-05-08 18:59:20 -0300

    creating view for register users

--- contact/forms.py
+++ contact/forms.py
@@ -1,6 +1,8 @@
 from typing import Any
 
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.forms import ValidationError
 
 from . import models
@@ -56,3 +58,56 @@ class ContactForm(forms.ModelForm):
             )
 
         return first_name
+
+
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+    last_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+    email = forms.EmailField(
+        required=True,
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name',
+            'last_name',
+            'email',
+            'username',
+            'password1',
+            'password2',
+        )
+
+    def clean(self) -> dict[str, Any]:
+        cleaned_data = super().clean()
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+
+        if first_name == last_name:
+            self.add_error(
+                'last_name',
+                ValidationError(
+                    'O primeiro nome não pode ser igual ao último nome',
+                    code='invalid_names'
+                )
+            )
+
+        return super().clean()
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            self.add_error(
+                'email',
+                ValidationError('Ja existe um usuario com este email',
+                                code='invalid_email')
+            )
+
+        return email
--- contact/views/__init__.py
+++ contact/views/__init__.py
@@ -1,2 +1,3 @@
 from .contact_forms import *  # noqa
 from .contact_views import *  # noqa
+from .user_forms import *  # noqa
